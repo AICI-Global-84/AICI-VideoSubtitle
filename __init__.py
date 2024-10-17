b76a04864cdf8d071bca95db3c39164100810972
AICI-Global-84 <aiciglobal01@gmail.com>
2024-10-18 01:28:02 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,12 @@
-from .SubtitleNode import SubtitleNode
+from .SubtitleNode import SubtitleNode, Timestamped_word
 NODE_CLASS_MAPPINGS = {
-    "SubtitleNode": SubtitleNode
+    "SubtitleNode": SubtitleNode,
+    "Timestamped_word": Timestamped_word
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "SubtitleNode": "Subtitle Node"
+    "SubtitleNode": "Subtitle Node",
+    "Timestamped_word": "Timestamped word"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
