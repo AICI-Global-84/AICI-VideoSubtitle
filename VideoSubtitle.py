dc9c14ea7b6498be2796368f8a19961f3d9b6a68
AICI-Global-84 <aiciglobal01@gmail.com>
2024-10-20 00:27:07 +0700

    Update VideoSubtitle.py

--- VideoSubtitle.py
+++ VideoSubtitle.py
@@ -181,8 +181,7 @@ class FormatSubtitles:
     def format_subtitles(self, transcript_file_name, is_upper=False, word_options_key="default"):
         self.logger.info(f'Formatting subtitles for transcript: {transcript_file_name}')
 
-        curr_json_dir = f'{JSON_DIR}/{transcript_file_name}'
-        transcript_json_path = f'{curr_json_dir}/{transcript_file_name}_transcript.json'
+        transcript_json_path = f'{JSON_DIR}/{transcript_file_name}_transcript.json'
 
         # Hàm này đọc file transcript JSON và chuyển đổi nó thành ma trận transcript
         def transcript_json_to_transcript_matrix(transcript_json_path):
